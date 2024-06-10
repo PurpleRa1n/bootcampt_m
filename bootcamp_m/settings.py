65536919f4261ec88f8053388c971919e524a09d
Artem Phinsky <artem.phinsky@gcorelabs.com>
2024-06-10 15:07:22 +0200

    Add ALLOWED_HOSTS = ['*']

--- bootcamp_m/settings.py
+++ bootcamp_m/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 0)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
